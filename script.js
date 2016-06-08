39f8563f5a5f5b6c0610451d17c8b90f33ea67a2
Faisal Khan <faisalkhan1222@gmail.com>
2016-06-08 12:36:39 -0400

    part1 finished

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    if(Cookies.get('name') !== undefined){
+      var greeting = document.createElement("h1");
+      var node = document.createTextNode("Welcome, " + Cookies.get('name'));
+      greeting.appendChild(node);
+      var element = document.getElementById("div1");
+      element.appendChild(greeting);
+    }
+}
+);
+
+function cookieTest(){
+name = document.getElementById('name').value;
+Cookies.set('name', name);
+location.reload();
+}
